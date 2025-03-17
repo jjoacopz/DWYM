293eefe48aed983fc625d0ac822a7f9287c1097a
jjoaco.pz <joacoperez.jpd@gmail.com>
2025-03-17 20:40:37 -0300

    JSON URL

---
+++ first.js
@@ -0,0 +1,10 @@
+async function main(){
+    const req = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+    if(req.ok){
+        const data = await req.json();
+        console.log(data);
+    }
+
+}
+
+main();
